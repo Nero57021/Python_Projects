486fae93ba01edb668f74f0e81f63a14c6af7d13
Jeremy Nero B <Nero57021@users.noreply.github.com>
2019-10-23 22:26:33 -0500

    Add files via upload

---
+++ Sequence types.py
@@ -0,0 +1,22 @@
+print("Test")
+
+s1 = """You are the creator
+and you should be happy with your creations
+It all starts with the beginning
+"""
+print(s1)
+# indexing
+print(s1[2])
+
+print(s1 * 3)
+
+print(len(s1))
+
+# slicing a string
+
+print(s1[0:21])
+
+print(s1[6:])
+
+
+#test pull
---
+++ random.py
@@ -0,0 +1,5 @@
+print ("test")
+i = 3
+while i <= 12:
+    print(i)
+    i+=1
