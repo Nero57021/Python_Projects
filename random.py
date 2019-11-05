5acb4afb220148ad2ab476cfd97dd3f127b99b68
Jeremy Nero B <Nero57021@users.noreply.github.com>
2019-11-05 14:54:08 -0600

    Added other py file

--- random.py
+++
@@ -1,5 +0,0 @@
-print ("test")
-i = 3
-while i <= 12:
-    print(i)
-    i+=1
--- test app.py
+++ test app.py
@@ -1,4 +1,9 @@
-import tools_to_try
+import simpletool
+from Student import Student
 
+student1 = Student("Jim", "Business", 3.1)
+print(student1)
+
+simpletool.roll_dice(3)
 print("test")
 
