9a4950f77817c2f04e72ace37e2404f7fe340caf
Jeremy Nero B <Nero57021@users.noreply.github.com>
2019-11-11 21:41:17 -0600

    Added other py file - one for a miniature question app

--- creation of list, tuple and dictionaries.py
+++
@@ -1,5 +0,0 @@
-# test
-print(" All succesful")
-print("test vcs")
-
-# test of Git again - configuration changes
--- file open - relative for metadata decrypt.py
+++
@@ -1,4 +0,0 @@
-employee_file = open("employee2.html", "a")
-employee_file.write("<p> This is a test of html<p>")
-
-employee_file.close()
